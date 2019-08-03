407e7b89170b689e6eb628de5f377ca6fd539fc4
DavidDev21 <davidzheng54@gmail.com>
2019-08-02 22:22:24 -0400

    Added a parallax. Tried adding profile card on top of parallax

--- site/src/App.vue
+++ site/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
+        <span>This is niceeeeee</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
@@ -16,18 +16,40 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <!-- Top most parallax -->
+      <!-- Not sure why my container doesn't grow once pass 1680px width-->
+      <!--
+      <v-parallax class="parallax_image elevation--0" :src="require('@/assets/NYC_background.jpg')">
+        <v-layout align-center justify-center row>
+          <v-flex>
+            <v-card class="mx-auto" min-height="300px" max-width="500px">
+              <v-layout>
+                <v-flex shrink>
+                  <v-img
+                    :src="require('@/assets/profile_picture.jpg')"
+                    height="200"
+                    width="200"
+                  >
+                  </v-img>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-parallax>
+      -->
+      <Main/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Main from './views/Main';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Main,
   },
   data: () => ({
     //
---
+++ site/src/views/Main.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-parallax class="parallax_image elevation--0" :src="require('@/assets/NYC_background.jpg')">
+      <v-layout align-center justify-center row>
+        <v-flex>
+          <v-card class="mx-auto" min-height="300px" max-width="900px">
+            <v-layout>
+              <v-flex shrink>
+                <v-img
+                  :src="require('@/assets/profile_picture.jpg')"
+                  height="200"
+                  width="200"
+                >
+                </v-img>
+              </v-flex>
+            </v-layout>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-parallax>
+    <v-container class="container_style">
+      <!-- Profile Card Section / Top Intro Section-->
+      <v-layout row wrap>
+          <v-flex>
+              <h4 class="test">WHAT HAPPENS TO THE CONTAINER</h4>
+          </v-flex>
+      <!-- Then follows Education-->
+
+      <!-- Experience -->
+
+      <!-- Projects -->
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+.test
+{
+  background: red;
+}
+.parallax_image
+{
+  width: 100%;
+  height: 100%;
+  opacity: .85;
+}
+
+.container_style
+{
+}
+</style>
+<script>
+export default {
+  data: () => ({
+  }),
+};
+</script>
