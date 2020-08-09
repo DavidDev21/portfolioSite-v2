0aece2bdfcdacce95f2c0f6ec6c9a44cfbdd6959
David Zheng <davidzheng54@gmail.com>
2020-08-09 00:39:54 -0400

    added comment to deploy.sh about ssh keys with wsl

--- site/deploy.sh
+++ site/deploy.sh
@@ -18,7 +18,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
+# if you are deploying to https://<USERNAME>.github.io ( need to fix linux subsystem's ssh problem with git)
+# TLDR: in the future, when you have SSH key for windows, you need to copy it for the linux subsystem as well
+# Its looking for those same keys in the linux system not the windows system.
 git push -f git@github.com:DavidDev21/daviddev21.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
